a9601288dd7b6aeb584f996fb88d40415771ac19
DevMichael22 <prcsambrose@gmail.com>
2022-02-24 10:00:18 -0800

    using style components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+import Home from './Components/Homescreen/Home'
+
+const App =()=>{
+
+  return(
+    <Home/>
+  )
+}
+
+export default App;
